89417fe1c1b8c89a42785be53e1b5aad0555f505
徐岱青 <email@xdq.me>
2019-02-17 21:18:13 +0800

    从环境导入数据设置

--- src/config.ts
+++ src/config.ts
@@ -11,4 +11,35 @@ let tConfig: tConfig = {
     },
 }
 
+//** load database config from env */
+if (process.env.DB_HOST && (process.env.DB_HOST.length > 0)) {
+    tConfig.database.host = process.env.DB_HOST;
+    console.log(`read DB_HOST from env`);
+}
+
+if (process.env.DB_PORT && (process.env.DB_PORT.length > 0)) {
+    try {
+        let port = parseInt(process.env.DB_PORT);
+        if (port > 0) {
+            tConfig.database.port = port
+            console.log(`read DB_PORT from env`);
+        }
+    } catch (e) { }
+}
+
+if (process.env.DB_DATABASE && (process.env.DB_DATABASE.length > 0)) {
+    tConfig.database.database = process.env.DB_DATABASE;
+    console.log(`read DB_DATABASE from env`);
+}
+
+if (process.env.DB_USERNAME && (process.env.DB_USERNAME.length > 0)) {
+    tConfig.database.username = process.env.DB_USERNAME;
+    console.log(`read DB_USERNAME from env`);
+}
+
+if (process.env.DB_PASSWORD && (process.env.DB_PASSWORD.length > 0)) {
+    tConfig.database.password = process.env.DB_PASSWORD;
+    console.log(`read DB_PASSWORD from env`);
+}
+
 export let config = tConfig
